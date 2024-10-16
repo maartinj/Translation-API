4e7dcdb727a4472714c0b74f024ad5b93230e434
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-10-16 23:58:04 +0200

    feat: AvailableLanguages Picker

---
+++ Translation Demo/TranslationService.swift
@@ -0,0 +1,13 @@
+//
+//  TranslationService.swift
+//  Translation Demo
+//
+//  Created by Marcin Jędrzejak on 16/10/2024.
+//
+
+import Foundation
+import Translation
+
+class TranslationService {
+    var translatedText = ""
+}
