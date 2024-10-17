2f9e5141c5f507869e4afeba6c5b70dd83d9d09c
Marcin Jędrzejak <mjedrzejak@icloud.com>
2024-10-17 12:54:53 +0200

    feat: Batch Translation all at once

--- Translation Demo/TranslationService.swift
+++ Translation Demo/TranslationService.swift
@@ -11,10 +11,18 @@ import Translation
 @Observable
 class TranslationService {
     var translatedText = ""
+    var reviews: [Review] = []
     var availableLanguages: [AvailableLanguage] =  []
     
     init() {
         getSupportedLanguages()
+        reviews = Review.samples
+    }
+    
+    func removeTranslations() {
+        reviews.indices.forEach { index in
+            reviews[index].translatedComment = ""
+        }
     }
     
     func getSupportedLanguages() {
@@ -30,5 +38,20 @@ class TranslationService {
     func translate(text: String, using session: TranslationSession) async throws {
         let response = try await session.translate(text)
         translatedText = response.targetText
+    }
+    
+    func translateAllAtOnce(using session: TranslationSession) async throws {
+        Task { @MainActor in
+            let requests: [TranslationSession.Request] = reviews.map { review in
+                    .init(sourceText: review.comment)
+            }
+            let responses = try await session.translations(from: requests)
+            let stringsToTranslate = responses.map { response in
+                response.targetText
+            }
+            for(index, translation) in stringsToTranslate.enumerated() {
+                reviews[index].translatedComment = translation
+            }
+        }
     }
 }
